507e932172c1d64a04bde16fc7b7c127f9aa0220
maryruthcathrynbsuello-spec <maryruthcathrynbsuello@gmail.com>
2026-06-11 22:09:34 +0800

    added functions for adding tasks

---
+++ script.js
@@ -0,0 +1,31 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.getElementById
+    ('task-input');
+    const addTaskButton = document.getElementById
+    ('add-task-btn');
+    const taskList = document.getElementById
+    ('tasks');
+
+
+    const addTask = (event) => {
+        event.preventDefault();
+        const taskText = taskInput.value.trim();
+        if(!taskText){
+            return;
+        }
+
+        const li = document.createElement('li');
+        li.textContent = taskText;
+        taskList.appendChild(li);
+        taskInput.value = '';
+    };
+
+    addTaskButton.addEventListener('click', addTask);
+
+    taskInput.addEventListener('keypress', (e) => {
+        if(e.key === 'Enter'){
+            addTask(e);
+        }
+    });
+});
