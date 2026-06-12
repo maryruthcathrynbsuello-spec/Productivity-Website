d73501bb16c9a4110aaaa05a4c11102a9369a750
maryruthcathrynbsuello-spec <maryruthcathrynbsuello@gmail.com>
2026-06-12 21:52:55 +0800

    added edit and delete buttons

--- script.js
+++ script.js
@@ -16,8 +16,14 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         const li = document.createElement('li');
-        li.innerHTML = `<input type="checkbox" class="task-checkbox"> <span>${taskText}</span>` ;
-
+        li.innerHTML = `<input type="checkbox" class="task-checkbox"> <span>${taskText}</span> 
+        <div class="task-buttons">
+            <button class="edit-btn"><i
+            class = "fa-solid fa-pen"></i></button>
+            <button class="delete-btn"><i
+            class = "fa-solid fa-trash"></i></button>
+        </div>
+        `;
         taskList.appendChild(li);
         taskInput.value = '';
     };
